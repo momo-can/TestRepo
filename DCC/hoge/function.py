b18926c52e8fd394b8d89fa6a19d09bd24af85b3
ShioMusubi <ohmomo.masahiro@gmail.com>
2021-10-13 17:11:42 +0900

    Update function.py

--- DCC/hoge/function.py
+++ DCC/hoge/function.py
@@ -1,3 +1,35 @@
+def getConfig(**kwargs):
+    department = kwargs.get('department', '')
+    if not department:
+        return
+
+    configPath = '/'.join([
+        os.path.dirname(__file__),
+        'Config',
+        '{}.json'.format(department)
+
+    ])
+
+    if not os.path.isfile(configPath):
+        return
+    fileId = open(configPath, 'r')
+    configData = json.load(fileId)
+    fileId.close()
+
+    print('> {}'.format(configPath))
+
+    return configData
+
+
+def getPluginDir():
+    d = os.path.dirname(__file__)
+    path = '/'.join([
+        d,
+        'Plugins'
+    ])
+    return path
+
+
 def main(**kwargs):
     configData = config.getConfig(**kwargs)
     recipes = configData.get('recipes', [])
